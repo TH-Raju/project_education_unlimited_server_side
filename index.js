76a44cc2e838aeba49e8511471071263e742ef40
TH-Raju <rajukhan894200@gmail.com>
2022-10-26 02:43:42 +0600

    server deploy on vercel

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ app.get('/', (req, res) => {
     res.send('API Running');
 });
 
+
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == '0') {
+        res.send(courses);
+    } else {
+        const selectCourse = courses.find(n => n.id == id);
+        res.send(selectCourse);
+    }
+
 });
 
 app.listen(port, () => {
