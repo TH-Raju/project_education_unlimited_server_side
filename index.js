d10ca748111de166ce69c7356e55c1481b799514
TH-Raju <rajukhan894200@gmail.com>
2022-10-25 17:40:53 +0600

    package added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const courses = require('./data/course.json');
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
-})
+});
 
 app.listen(port, () => {
     console.log('Server Running...')
